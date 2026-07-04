9fea58bbf3338ffbca45649423039630dc78ea0f
mbaxsrl8 <a10115307@gmail.com>
2026-07-04 16:32:23 +0800

    Implement solution for "Best Time to Buy and Sell Stock" problem

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = 0
+        sell = 1
+        max_profit = 0
+        while sell < len(prices):
+            profit = prices[sell] - prices[buy]
+            if profit > 0:
+                sell = sell + 1
+                if profit > max_profit:
+                    max_profit = profit
+            else:
+                buy = buy + 1
+                if sell == buy:
+                    sell = sell + 1
+        return max_profit
+    
+
+if '__main__' == __name__:
+    sol = Solution()
+    print(sol.maxProfit([7,1,5,3,6,4]))
