2c028c4066eee3778917e19fb88a6f7a9b11f0b7
mbaxsrl8 <a10115307@gmail.com>
2026-07-04 22:36:44 +0800

    add maxium_subarray.py

---
+++ maxium_subarray.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        i = 0
+        res = nums[0]
+        while i < len(nums):
+            j = i
+            prefix = 0
+            while j < len(nums):
+                if prefix < 0:
+                    i = j
+                    break
+                res = max(res, prefix + nums[j])
+                prefix = prefix + nums[j]
+                j = j + 1
+                if j == len(nums):
+                    i = j
+        return res
+            
+            
+if "__main__" == __name__:
+    sol = Solution()
+    print(sol.maxSubArray([-1]))
