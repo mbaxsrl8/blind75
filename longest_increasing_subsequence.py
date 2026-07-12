d0c5a18522ff415b891426361a5f616ddcc117cd
mbaxsrl8 <a10115307@gmail.com>
2026-07-12 11:24:16 +0800

    update longest_increasing_subsequence.py

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -4,18 +4,13 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        res = 1
         dp = [1] * len(nums)
         i = len(nums) - 2 
-        while i >= 0:
-            max_suffix = 0
+        for i in range(len(nums) - 2, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[j] > nums[i]:
-                    max_suffix = max(max_suffix, dp[j])
-            dp[i] += max_suffix
-            res = max(res, dp[i])
-            i -= 1
-        return res
+                    dp[i] = max(dp[i], 1 + dp[j])     
+        return max(dp)
             
         
     # def lengthOfLIS(self, nums: List[int]) -> int:
