cff5c0706121fc25d8be8c8549c796d074ca7378
mbaxsrl8 <a10115307@gmail.com>
2026-07-19 12:10:32 +0800

    update remove_nth_node_from_end_of_list.py

--- remove_nth_node_from_end_of_list.py
+++ remove_nth_node_from_end_of_list.py
@@ -6,19 +6,35 @@ from linked_list import ListNode, convert_list_to_linked_list
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        end = self.reverseList(head)
-        dummy = ListNode(0, end)
-        p = dummy
+        dummy = ListNode(0, head)
+        l = dummy
+        r = l
         i = 0
-        while i < n - 1:
-            p = p.next
-            i = i + 1
+        while i < n:
+            r = r.next
+            i += 1
+        while r.next:
+            l = l.next
+            r = r.next
+
+        next_node = l.next.next
+        l.next.next = None
+        l.next = next_node 
+        return dummy.next
+    # def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    #     end = self.reverseList(head)
+    #     dummy = ListNode(0, end)
+    #     p = dummy
+    #     i = 0
+    #     while i < n - 1:
+    #         p = p.next
+    #         i = i + 1
         
-        next_node = p.next.next
-        p.next.next = None
-        p.next = next_node
+    #     next_node = p.next.next
+    #     p.next.next = None
+    #     p.next = next_node
         
-        return self.reverseList(dummy.next)
+    #     return self.reverseList(dummy.next)
 
         
         
